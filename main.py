ff727c022cfe6c56a266313049692a5cb4c412ec
imijanur <mijanurtj@gmail.com>
2019-07-22 12:56:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import Flask
-from nsetools import New
+from nsetools import Nse
 from nsepy import history_data
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "deploy"
+
+if __name__ = "__main__" :
+    app.run()
